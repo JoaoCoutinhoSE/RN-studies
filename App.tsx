53e3eaccc2bcb2df1a8463ce0ecb3b72fc77135b
PandaZIM <jaotoledo74@gmail.com>
2021-12-19 17:05:43 -0300

    nav

--- App.tsx
+++ App.tsx
@@ -1,23 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { Routes } from "./src/navigation";
 
 export default function App() {
   return (
-    <View>
-      <NavigationContainer>
-        <Routes />
-      </NavigationContainer>
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
-
-/*const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});*/
